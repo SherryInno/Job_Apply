34cb98d01d3d00d3921b22bca338f616438f3b40
Sherry Innocent <sher47.si@gmail.com>
2026-03-11 10:40:05 -0400

    Rename job_apply.js to job_apply.jsx and update import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import JobApplyTool from "../job_apply.js";
+import JobApplyTool from "../job_apply.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
